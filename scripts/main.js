1fe77223444fb0e398fa2fed3031e41830ae6344
MatheoL <matheoledesma1@gmail.com>
2021-09-23 18:28:44 -0300

    anlcas

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,37 @@
+//----------- boton menu activar-----------
 var btnMenu = document.getElementById('btn-menu')
 var nav = document.getElementById('nav')
 
 btnMenu.addEventListener('click', function() {
     nav.classList.toggle('show')
+})
+// ----------- off -----------
+nav.addEventListener('click', ()=>{
+    nav.classList.remove('show')
+})
+//--------------------------------------------------
+//-------------------------- Selected nav--------------------------
+ 
+
+//--------------------------------------------------
+//-------------------------- Boton  Vamos--------------------------
+var btnGo = document.getElementById('btn-go')
+var altoBanner = document.getElementById('banner')
+
+btnGo.addEventListener('click', function(e){
+    e.preventDefault()
+    window.scrollTo({
+        top:  altoBanner.clientHeight,
+        behavior: 'smooth'
+      });
+})
+//--------------------------------------------------
+//-------------------------- Boton  Inicio (logo)--------------------------
+var logo = document.getElementById('logo')
+
+logo.addEventListener('click', ()=>{
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
 })
